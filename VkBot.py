b181edfe062bb3effa05de88b82a78ee90a36a20
Nikita Antonenkov <nikita2102antonenkov@gmail.com>
2024-03-17 20:02:41 +0300

    Completed code refactoring

--- VkBot.py
+++ VkBot.py
@@ -5,6 +5,11 @@ from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api import VkUpload
 from enum import Enum
 
+DEANERY_MESSAGE = """❕Кабинеты деканата располагаются в УЛК на 7 этаже.\n
+    Декан: Ремарчук Валерий Николаевич (кабинет 703л)\n
+    Зам. декана по молодёжной политике\n
+    и воспитательной деятельности: Гаврилова Юлия Викторовна"""
+
 
 class VkBotState(Enum):
     INIT_STATE = 0
@@ -14,47 +19,25 @@ class VkBotState(Enum):
     SHS4_CLICKED = 4
 
 
-def get_keyboard_1():
-    settings = dict(one_time=False, inline=True)
-    keyboard_1 = VkKeyboard(**settings)
-    keyboard_1.add_callback_button(label='СГН1', color=VkKeyboardColor.PRIMARY, payload={"type": "SHS1"})
-    keyboard_1.add_line()
-    keyboard_1.add_callback_button(label='СГН2', color=VkKeyboardColor.PRIMARY, payload={"type": "SHS2"})
-    keyboard_1.add_line()
-    keyboard_1.add_callback_button(label='СГН3', color=VkKeyboardColor.PRIMARY, payload={"type": "SHS3"})
-    keyboard_1.add_line()
-    keyboard_1.add_callback_button(label='СГН4', color=VkKeyboardColor.PRIMARY, payload={"type": "SHS4"})
-    keyboard_1.add_line()
-    keyboard_1.add_callback_button(label='Деканат', color=VkKeyboardColor.POSITIVE, payload={"type": "DEANERY"})
+def upload_photo(vk_session, photo):
+    upload = VkUpload(vk_session)
+    response = upload.photo_messages(photo)[0]
+    owner_id = response['owner_id']
+    photo_id = response['id']
+    access_key = response['access_key']
+    return owner_id, photo_id, access_key
 
 
-def get_keyboard_2():
-    settings = dict(one_time=False, inline=True)
-    keyboard_2 = VkKeyboard(**settings)
-    keyboard_2.add_callback_button(label='Заведующий', color=VkKeyboardColor.POSITIVE, payload={"type": "HEAD_OF_DEPARTMENT"})
-    keyboard_2.add_line()
-    keyboard_2.add_callback_button(label='Научная работа', color=VkKeyboardColor.PRIMARY, payload={"type": "SCIENTIFIC_WORK"})
-    keyboard_2.add_line()
-    keyboard_2.add_callback_button('Назад', color=VkKeyboardColor.NEGATIVE, payload={"type": "BACK"})
-
-def upload_photo(vk, photo):
-        upload = VkUpload(vk)
-        response = upload.photo_messages(photo)[0]
-        owner_id = response['owner_id']
-        photo_id = response['id']
-        access_key = response['access_key']
-
-        return owner_id, photo_id, access_key
-
-def send_message(vk, peer_id, owner_id, photo_id, access_key, message):
-    attachment = f'photo{owner_id}_{photo_id}_{access_key}'
-    vk.messages.send(
-    random_id=get_random_id(),
-    peer_id=peer_id,
-    message=message,
-    attachment=attachment
-)
-
+def init_state_keyboard():
+    keyboard = VkKeyboard(one_time=False, inline=True)
+    keyboard.add_callback_button(label='СГН1', color=VkKeyboardColor.PRIMARY, payload={"type": "SHS1"})
+    keyboard.add_callback_button(label='СГН2', color=VkKeyboardColor.PRIMARY, payload={"type": "SHS2"})
+    keyboard.add_line()
+    keyboard.add_callback_button(label='СГН3', color=VkKeyboardColor.PRIMARY, payload={"type": "SHS3"})
+    keyboard.add_callback_button(label='СГН4', color=VkKeyboardColor.PRIMARY, payload={"type": "SHS4"})
+    keyboard.add_line()
+    keyboard.add_callback_button(label='Деканат', color=VkKeyboardColor.POSITIVE, payload={"type": "DEANERY"})
+    return keyboard
 
 
 class VkBot:
@@ -63,6 +46,9 @@ class VkBot:
         self.vk_session = vk_api.VkApi(token=group_token)
         self.vk = self.vk_session.get_api()
         self.longpoll = VkBotLongPoll(self.vk_session, group_id=group_id)
+        self.attachments = {
+            'init_state': upload_photo(self.vk_session, 'resources/sgn.jpg')
+        }
 
     def start(self):
         for event in self.longpoll.listen():
@@ -89,20 +75,42 @@ class VkBot:
 
             self.shs4_clicked_handler(event)
 
-
     def init_state_handler(self, event):
-        self.vk.messages.send(user_id=event.obj.message['from_id'], random_id=get_random_id(), peer_id=event.obj.message['from_id'], keyboard=self.get_keyboard_1(), message=event.obj.message['text'])
-        if event.type == VkBotEventType.MESSAGE_EVENT:
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            owner_id, photo_id, access_key = self.attachments['init_state']
+            attachment = f'photo{owner_id}_{photo_id}_{access_key}'
+            self.vk.messages.send(
+                user_id=event.obj.message['from_id'],
+                random_id=get_random_id(),
+                peer_id=event.obj.message['from_id'],
+                keyboard=init_state_keyboard().get_keyboard(),
+                message=event.obj.message['text'],
+                attachment=attachment
+            )
+        elif event.type == VkBotEventType.MESSAGE_EVENT:
             if event.object.payload.get('type') == 'SHS1':
+
                 self.state = VkBotState.SHS1_CLICKED
+                self.shs1_clicked_handler(event)
+
             elif event.object.payload.get('type') == 'SHS2':
+
                 self.state = VkBotState.SHS2_CLICKED
+                self.shs2_clicked_handler(event)
+
             elif event.object.payload.get('type') == 'SHS3':
+
                 self.state = VkBotState.SHS3_CLICKED
+                self.shs3_clicked_handler(event)
+
             elif event.object.payload.get('type') == 'SHS4':
+
                 self.state = VkBotState.SHS4_CLICKED
+                self.shs4_clicked_handler(event)
+
             elif event.object.payload.get('type') == 'DEANERY':
-                deanery_clicked_handler(self, event)
+
+                self.deanery_clicked_handler(event)
 
     def shs1_clicked_handler(self, event):
         pass
@@ -117,11 +125,10 @@ class VkBot:
         pass
 
     def deanery_clicked_handler(self, event):
-        last_id = self.vk.messages.edit(peer_id=event.obj.peer_id, message='❕Кабинеты деканата располагаются в УЛК на 7 этаже.\n Декан: Ремарчук Валерий Николаевич (кабинет 703л)\nЗам. декана по молодёжной политике\n и воспитательной деятельности: Гаврилова Юлия Викторовна', conversation_message_id=event.obj.conversation_message_id)
-
-    
-        
-
-
-
-
+        last_id = self.vk.messages.edit(
+            peer_id=event.obj.peer_id,
+            message=DEANERY_MESSAGE,
+            conversation_message_id=event.obj.conversation_message_id,
+            keyboard=init_state_keyboard().get_keyboard(),
+            attachment=self.attachments['init_state']
+        )
