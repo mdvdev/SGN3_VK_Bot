4dc67a741b2f13e3f957ac1d7bad2527bd61ef40
tamakulova <149898802+tamakulova@users.noreply.github.com>
2024-03-17 16:31:30 +0300

    Update VkBot.py

--- VkBot.py
+++ VkBot.py
@@ -2,7 +2,6 @@ import vk_api
 from vk_api.utils import get_random_id
 from vk_api.bot_longpoll import VkBotEventType, VkBotLongPoll
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
-import json
 from vk_api import VkUpload
 from enum import Enum
 
